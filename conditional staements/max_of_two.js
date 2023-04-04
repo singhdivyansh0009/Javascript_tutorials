876a94eb2b281b54303338345e7234887e419948
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2023-04-05 00:26:01 +0530

    Add files via upload

---
+++ conditional staements/max_of_two.js
@@ -0,0 +1,8 @@
+/*program to find max of two */
+
+let a=prompt("enter first number");
+let b=prompt("enter first number");;
+if(a>b)
+    console.log(a ,"is greater");
+else
+    console.log(b, "is greater");
---
+++ conditional staements/vowel_check.js
@@ -0,0 +1,6 @@
+/* program to check a letter is vowel or consonant */
+let ch = prompt("enter a character");
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        console.log("character is vowel");
+    else
+        console.log("character is consonant");
