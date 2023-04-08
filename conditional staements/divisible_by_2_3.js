3b7748e5ea06ac928ac38cc0d9e48d879feae449
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2023-04-08 23:33:22 +0530

    Create divisible_by_2_3.js

---
+++ conditional staements/divisible_by_2_3.js
@@ -0,0 +1,7 @@
+/* program to check divisiblity */
+
+let num = prompt("enter a number");
+if(num%2==0||num%3==0)
+    console.log("divisible by 2 or 3");
+else
+    console.log("not divisible by 2 or 3");
