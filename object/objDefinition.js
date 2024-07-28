83e6d87dcc48ab8963b303eb043165d7b8660fcc
singhdivyansh0009 <=>
2024-07-28 17:34:01 +0530

    updated objDefinition with object constrcutor topic

--- object/objDefinition.js
+++ object/objDefinition.js
@@ -1,5 +1,4 @@
-/* definition using new keyword and 
-   creating instance of Object class using its constructor */
+// 1.definition using new keyword 
 const person = new Object();
 person.name = "Ram";
 person.age = 40;
@@ -7,14 +6,32 @@ console.log(person);
 
 // const person  = Object();  it will not a correct way to create an instance , it may run on specfic enviroment
 
-// definition of object using Object literal
+// 2.definition of object using Object literal
 const car = {
     brand: 'Toyota',
     model: 'Camry',
     year: 2015,
     price: 500000
 }
+car.colour = "red";  // we can also add properties after the creation of object
 console.log(car);
+
+// 3. Definition using constructor
+function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.eat = function(){
+        console.log("Eating");
+    }
+}
+
+// Creating objects using the constructor
+const person1 = new Person("John", "Doe", 30);
+const person2 = new Person("Jane", "Smith", 25);
+person1.gender = "male";  // this statement add properties in person1 object not in Person constructor
+console.log(person1.eat());
+console.log(person2);
 
 // object contains both the properties and methods
 const bus = {
