76ca1287484fa73160a82d8c9cec3eac2597309d
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2024-09-09 15:18:48 +0530

    Create findMethod.js
    added find method

---
+++ Array/findMethod.js
@@ -0,0 +1,4 @@
+// array find method took a callback for each value array 
+// if condition satisfied return true else return false
+const arr = [1,2,3,4,5];
+const result = arr.find((num) => num === 3);
