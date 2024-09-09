c37c68009e46b722dadcdd5fbfb48254702e7ff2
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2024-09-09 15:21:32 +0530

    Update findMethod.js

--- Array/findMethod.js
+++ Array/findMethod.js
@@ -1,4 +1,5 @@
 // array find method took a callback for each value array 
-// if condition satisfied return true else return false
+// if condition satisfied return the first number that satisfied the condition
+//  else return undefined
 const arr = [1,2,3,4,5];
 const result = arr.find((num) => num === 3);
